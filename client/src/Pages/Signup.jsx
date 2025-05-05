dc541a1fc478f7ed308476d657d55cf15064c5a9
hanzla631212 <2470@students.riphah.edu.pk>
2025-05-05 17:28:32 +0500

    ..

--- client/src/Pages/Signup.jsx
+++ client/src/Pages/Signup.jsx
@@ -1,5 +1,32 @@
+import React from 'react';
+import './Signup.css';
+import founder3 from "../assets/download.png";
 function Signup() {
-    return <h2>Signup Page</h2>;
-  }
-  export default Signup;
-  
+  return (
+    <div className="signup-container">
+      <div className="signup-form">
+        <h2>Sign up</h2>
+        <form>
+          <input type="text" placeholder="John Doe" />
+          <input type="email" placeholder="Your Email" />
+          <input type="password" placeholder="Password" />
+          <input type="password" placeholder="Repeat your password" />
+          <div className="input-wrapper">
+  {/* <label className="terms">
+    <input type="checkbox" />
+    <span>I agree all statements in <a href="#">Terms of service</a></span>
+  </label> */}
+</div>
+
+          <button type="submit">Register</button>
+        </form>
+      </div>
+      <div className="signup-image">
+        <img src= {founder3} alt="desk illustration" />
+        <p className="already-member">I am already member</p>
+      </div>
+    </div>
+  );
+}
+
+export default Signup;
